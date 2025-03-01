9bb60f752f8369c4e77669b23e89d9d0eab2e7db
Kxvxn-sj <kev_dd@icloud.com>
2025-03-01 10:40:34 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+
+  document.querySelector(".excuse").innerHTML = GenerateExcuse();
+  console.log("Hello Rigo from the console!");  
+
 };
+
+let GenerateExcuse = () => {
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let whoindex = Math.floor (Math.random() * who.length);
+  let actionindex = Math.floor (Math.random() * action.length);
+  let whatindex = Math.floor (Math.random() * what.length);
+  let whenindex = Math.floor (Math.random() * when.length);
+
+  return who[whoindex] +' '+ action[actionindex] +' '+ what[whatindex] +' '+ when[whenindex];
+}
+
